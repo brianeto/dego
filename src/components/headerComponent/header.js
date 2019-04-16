ebfa6066a0cfb59555f35276a04a5b5fce021315
Ninjadrop <bdmb45@gmail.com>
2019-04-16 07:32:10 -0500

    First commit

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        
+        <div className="logo">
+            LOGOXD
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+            </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
